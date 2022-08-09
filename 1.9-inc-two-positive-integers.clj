b941660c376dfe3fd26b59f86047000092912115
Illia Rayetzki <rayetzkiillya@gmail.com>
2022-08-09 12:50:49 +0300

    [1.9-1.10]: Recursion and Iteration

---
+++ 1.9-inc-two-positive-integers.clj
@@ -0,0 +1,22 @@
+(defn inc-first [a b]
+  (if (= a 0) b (inc (inc-first (dec a) b))))
+
+;; (inc-first 4 5)
+;; (inc (inc-first 3 5))
+;; (inc (inc (inc-first 2 5)))
+;; (inc (inc (inc (inc-first 1 5))))
+;; (inc (inc (inc (inc 5))))
+;; (inc (inc (inc 6)))
+;; (inc (inc 7))
+;; (inc 8)
+;; 9 <-- recursive
+
+(defn inc-second [a b]
+  (if (= a 0) b (inc-second (dec a) (inc b))))
+;; (inc-second 4 5)
+;; (inc-second 3 6)
+;; (inc-second 2 7)
+;; (inc-second 1 8)
+;; (inc-second 0 9)
+;; 9 <-- iterative
+
